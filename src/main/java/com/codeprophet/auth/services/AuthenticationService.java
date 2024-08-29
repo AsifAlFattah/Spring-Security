419afe1b921d836473f403087094d77dd3616a8c
Asif Al Fattah <asifalfattah34@gmail.com>
2024-08-29 18:20:10 +0600

    Completed

---
+++ src/main/java/com/codeprophet/auth/services/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.codeprophet.auth.services;
+
+import com.codeprophet.auth.auth.AuthenticationResponse;
+import com.codeprophet.auth.auth.AutheticationRequest;
+import com.codeprophet.auth.auth.RegisterRequest;
+import com.codeprophet.auth.user.Role;
+import com.codeprophet.auth.user.User;
+import com.codeprophet.auth.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoderencoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoderencoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AutheticationRequest request) {
+        authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
